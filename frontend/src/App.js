71d839ad34a0395cf00b680143630d766a5283fb
Sarah Spriggs <sarahspriggs@Sarahs-MacBook-Pro.local>
2021-09-11 23:45:31 -0400

    Started building bmi calculator.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,21 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-      </header>
+    <div className="bmi-calculator">
+              <h1 className="bmi-calculator-title text-center mt-5 mb-5">BMI Calculator</h1>
+      <form className="bmi-calculator-container border-1">
+          <span className="text-center mt-1 mb-2">Imperial or Metric</span>
+          <div className="input-type">
+            <div className="input-container">
+              <input type="radio" id="imperial" name="input-type" value="imperial"/>
+              <label for="imperial">Imperial</label>
+            </div>
+            <div className="input-container">
+              <input type="radio" id="metric" name="input-type" value="metric"/>
+              <label for="metric">Metric</label>
+            </div>
+          </div>
+      </form>
     </div>
   );
 }
