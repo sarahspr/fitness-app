cf6912ec7c35a557718731f6a95c9b0f332c94fe
Sarah <sarah1591@gmail.com>
2021-09-17 17:27:19 -0400

    Started building basic functionality for TDEE Calculator.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BmiCalculator from './BmiCalculator';
 import BmrCalculator from './BmrCalculator';
+import TdeeCalculator from './TdeeCalculator';
 
 ReactDOM.render(
   <React.StrictMode>
     <BmiCalculator />
     <BmrCalculator />
+    <TdeeCalculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
