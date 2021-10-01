7dbf6cf96298fe6c856150620993cbb542a1ecde
Sarah <sarah1591@gmail.com>
2021-10-01 16:05:09 -0400

    Setup database connection.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-const port = 3000;
+var express = require("express");
+const { MongoClient } = require("mongodb");
+var path = require("path");
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname + "/../frontend/public/index.html"));
+var app = express();
+
+//DB Config
+const db = require("./config.env").mongoURI;
+const client = new MongoClient(db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Connect to Mongo
+client
+	.connect()
+	.then(() => console.log("Mongo Connected"))
+	.catch((err) => console.log(err));
+
+app.get("/", (request, response) => {
+	response.sendFile(path.join(__dirname + "/../frontend/public/index.html"));
 });
 
+//Setup Port for Server to Run on
+
+const port = 3000;
+
 app.listen(port, () => {
-  console.log(`Fitness App is listening at http://localhost:${port}`);
+	console.log(`Fitness App is listening at http://localhost:${port}`);
 });
